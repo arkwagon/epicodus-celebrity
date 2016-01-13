f245c6b63629fe532c9ad93a1a85f5aa7a606f96
Hillary Hintzen and Will Murphy <hillary07@gmail.com>
2016-01-13 11:48:16 -0800

    create initial content and scripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#blanks").submit(function(event) {
+    var betty = ("input#gender").val();
+      if (gender === "female" && age >= 35) {
+      (#output5(.show))
+      }
+
+    event.preventDefault();
+  });
+});
