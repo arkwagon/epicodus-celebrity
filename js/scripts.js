9d6d34487c41a5c93b58e3c63716f99a8b436a09
Hillary Hintzen and Will Murphy <hillary07@gmail.com>
2016-01-13 13:58:46 -0800

    update scripts to reflect blank field error and complete HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
 $(document).ready(function() {
-  $("form#blanks").submit(function(event) {
-    var betty = ("input#gender").val();
-      if (gender === "female" && age >= 35) {
-      (#output5(.show))
+  $("form#celebrity").submit(function(event) {
+    console.log('test');
+    var gender = $("input#gender").val();
+    var age = parseInt($("input#age").val());
+      if (gender === "women" && age <= 35) {
+       $("#output5").show();
+     }
+
+    var gender = $("input#gender").val();
+    var age = parseInt($("input#age").val());
+      if (gender === "women" && age >= 35) {
+        $("#output4").show();
       }
+
+    var gender = $("input#gender").val();
+    var age = parseInt($("input#age").val());
+      if (gender === "men" && age <= 35) {
+        $("#output3").show();
+      }
+
+    var gender = $("input#gender").val();
+    var age = parseInt($("input#age").val());
+      if (gender === "men" && age >= 35) {
+        $("#output2").show();
+      } else {
+       alert('Please fill all fields!');
+     }
+
+
 
     event.preventDefault();
   });
